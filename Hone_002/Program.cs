53212ed67883a28863127df6908efd9138c70da7
Serge Bogoduhov <zz9250347500@gmail.com>
2023-04-17 15:37:20 +0300

    DZ_002 nach

--- Hone_002/Program.cs
+++ Hone_002/Program.cs
@@ -7,3 +7,22 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
+
+
+Nach();
+
+void Nach()
+{
+    int rows = ("rows");
+    int columns = ("columns");
+
+}
+
+int Bass(String msg)
+{
+    System.Console.WriteLine(msg);
+    String.ReadInpute = System.Console.ReadLine();
+    int result = int.Parse(ReadInpute);
+    return result;
+}
+
